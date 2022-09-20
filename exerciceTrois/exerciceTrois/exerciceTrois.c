4b7053df47579481cbe25a4f2afe580ea396c85c
Arnaud Derison <arnaud.derison@hotmail.com>
2022-09-20 22:39:32 +0200

    exercice de saisies clavier. Attention error dans le double

---
+++ exerciceTrois/exerciceTrois/exerciceTrois.c
@@ -0,0 +1,68 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    double poids;
+
+    printf("Quel est votre poids ?");
+    poids = lireDouble();
+
+    printf("Votre poids est  de %f Kg", poids);
+    return 0;
+}
+
+int lire(char *chaine, int longueur) {
+    char* positionToucheEnter = NULL;
+    char* positionVirgule = NULL;
+
+    if (fgets(chaine, longueur, stdin) != NULL) //la saisie est converti ?
+    {
+        //trouver '\n"
+        positionToucheEnter = strchr(chaine, "\n");
+        positionVirgule = strchr(chaine, ",");
+
+        if (positionVirgule != NULL) {
+            *positionVirgule = ".";
+        }
+
+        if (positionToucheEnter != NULL)
+        {
+            *positionToucheEnter = "\0"; //change "\n" en "\0" pour supprimer le retour à la ligne
+            return 1;
+        }
+        else {
+            viderBuffer();
+        }
+    }
+    else {
+        viderBuffer();
+        return 0;
+    }
+}
+
+int viderBuffer()
+{
+    int c = 0;
+    while (c != '\n' && c != EOF)
+    {
+        return c = getchar();
+    }
+}
+
+int lireDouble()
+{
+    char nombreText[100]; //100 devrait suffir selon OpenClasroom
+    
+    if (lire(nombreText, 100)) {
+        //char* endptr;
+        //lire le text sans "\n" et convertir en nombre
+        //return strtol(nombreText, NULL, 10);
+        return strtod(nombreText, NULL);
+    }
+    else {
+        return 0;
+    }
+}
